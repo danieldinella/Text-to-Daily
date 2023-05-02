c5a04da77f078dd7e31ef8c4fb1ae80b6902cbfb
danieldinella <65407743+danieldinella@users.noreply.github.com>
2023-05-02 18:05:42 +0000

    Dichiarazione e inizializzazione variabili e file.

---
+++ Main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+
+int dist, width, lines, columns, line_l;
+FILE *fin, *fout;
+
+fin = fopen("input.txt", "r");
+fout = fopen("output.txt", "w");
+
+printf("Inserire i parametri e premere invio:\n");
+
+printf("Righe di ogni colonna:\n");
+scanf("%d", lines);
+
+printf("Caratteri per ogni colonna:\n");
+scanf("%d", width);
+
+printf("Numero massimo di colonne per pagina:\n");
+scanf("%d", columns);
+
+printf("Distanza tra ogni colonna:\n");
+scanf("%d", dist);
+
+char line[width];
+
+while (!feof(fin)){
+
+    fgets(line,width,fin);
+    line_l=strlen(line);
+	if (line[line_l-1]=='\n'){
+        if (line[line_l-2]=='\n'){
+            
+        }
+        line[line_l-1]='\0';
+    }
+	if (line[line_l-1]!='\n')
+		line[line_l-1]='\n';
+
+	fputs(line,fout);
+}
+
+}
