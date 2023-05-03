93e1030e436b3f8b7207f90f2381f0a169e79438
danieldinella <65407743+danieldinella@users.noreply.github.com>
2023-05-03 14:49:24 +0000

    Aggiunti commenti e controllo su fine paragrafo.

--- Main.c
+++ Main.c
@@ -1,46 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 
 int main(){
 
-int dist, width, lines, columns, line_l;
-FILE *fin, *fout;
+    //Dichiarazione e inizializzazione parametri e file
+    int dist, width, lines, columns, line_l;
+    FILE *fin, *fout;
 
-fin = fopen("input.txt", "r");
-fout = fopen("output.txt", "w");
+    fin = fopen("input.txt", "r");
+    fout = fopen("output.txt", "w");
 
-printf("Inserire i parametri e premere invio:\n");
+    printf("Inserire i parametri e premere invio:\n");
 
-printf("Righe di ogni colonna:\n");
-scanf("%d", lines);
+    printf("Righe di ogni colonna:\n");
+    scanf("%d", lines);
 
-printf("Caratteri per ogni colonna:\n");
-scanf("%d", width);
+    printf("Caratteri per ogni colonna:\n");
+    scanf("%d", width);
 
-printf("Numero massimo di colonne per pagina:\n");
-scanf("%d", columns);
+    printf("Numero massimo di colonne per pagina:\n");
+    scanf("%d", columns);
 
-printf("Distanza tra ogni colonna:\n");
-scanf("%d", dist);
+    printf("Distanza tra ogni colonna:\n");
+    scanf("%d", dist);
 
-char line[width];
 
-while (!feof(fin)){
+    //Lettura del file dato in input
+    char line[width];
+    bool endp = false;
 
-    fgets(line,width,fin);
-    line_l=strlen(line);
-	if (line[line_l-1]=='\n'){
-        if (line[line_l-2]=='\n'){
-            
+    while (!feof(fin)) {
+        
+        //Se la riga prima era l'ultima di un paragrafo salta una riga altrimenti procedi con la lettura
+        if (endp) {
+            fputs("\n",fout);
+            endp = false;
         }
-        line[line_l-1]='\0';
-    }
-	if (line[line_l-1]!='\n')
-		line[line_l-1]='\n';
+        else {
+            fgets(line,width,fin);
+            line_l=strlen(line);
+            if (line[line_l-1]=='\n'){
+                if (line[line_l-2]=='\n'){
+                    endp = true;
+                }
+                line[line_l-1]='\0';
+            }
+            if (line[line_l-1]!='\n')
+                line[line_l-1]='\n';
 
-	fputs(line,fout);
-}
+            fputs(line,fout);
+        }
+    }
 
 }
